d8b5f702bf6ef4e6882f2746fe08ba4b084fa911
Joey Schrader <jschrader@overstock.com>
2021-02-09 08:08:17 -0800

    React router skeleton init

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,58 @@
 import React from 'react'
-import 'joey/dist/index.css'
+import { Switch, Route, NavLink, withRouter } from 'react-router-dom';
+// import 'joey/dist/index.css'
 import Buttons from './Buttons/Buttons'
 import Section from './Section/Section'
 import Accordions from './Accordions/Accordions'
 
-const App = () => {
+export default function App() {
   return (
     <div className="container">
-      <div>
-        <Buttons />
-        <Section />
-        <Accordions />
-      </div>
+      <SideBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/buttons" component={Buttons} />
+        <Route path="/section" component={Section} />
+        <Route path="/accordions" component={Accordions} />
+      </Switch>
     </div>
   )
 }
 
-export default App
+function Home() {
+  return (
+    <div>
+      I will be the home page
+    </div>
+  )
+}
+
+function SideBar() {
+  const navLinks = [
+    {
+      name: 'buttons',
+      path: '/buttons'
+    },
+    {
+      name: 'section',
+      path: '/section'
+    },
+    {
+      name: 'accordions',
+      path: '/accordions'
+    },
+  ]
+  return (
+    <nav>
+      {navLinks.map((nav) => {
+        return (
+          <div key={nav.name}>
+            <NavLink to={nav.path}>
+              {nav.name}
+            </NavLink>
+          </div>
+        );
+      })}
+    </nav>
+  )
+}
