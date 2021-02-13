d222347602a90cab2c0c6341482ea353bf10088c
Joey Schrader <jschrader@overstock.com>
2021-02-13 12:39:37 -0800

    Add basename to BrowserRouter

--- example/src/index.js
+++ example/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/joeys-components">
         <App />
     </BrowserRouter>, document.getElementById('root')
 )
